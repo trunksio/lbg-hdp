3ba6e669b7cb079651afb627d33a83f3432e1bfb
Lewis Crawford <lewis@Lewiss-MacBook-Pro-2.local>
2018-05-29 16:14:06 +0100

    added yarn / spark

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 curl -v -k -u admin:admin -H "X-Requested-By:ambari" -X POST http://localhost:8080/api/v1/version_definitions  -d '{"VersionDefinition": {"version_url": "file:/tmp/HDP-2.6.4.0-91.xml" } }'
-curl --user admin:admin -H 'X-Requested-By:admin' -X POST http://localhost:8080/api/v1/blueprints/hdfs --data-binary @examples/blueprints/hdfs.json
+curl --user admin:admin -H 'X-Requested-By:admin' -X POST http://localhost:8080/api/v1/blueprints/hdfs --data-binary @examples/blueprints/hdfsyarn.json
 curl --user admin:admin -H 'X-Requested-By:admin' -X POST localhost:8080/api/v1/clusters/dev --data-binary @examples/hostgroups/hdfs.json
 curl --user admin:admin -H 'X-Requested-By:admin'  -X POST localhost:8080/api/v1/clusters/dev/hosts/ambari-server.lbg.dev
