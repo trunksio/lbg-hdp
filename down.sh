4f72a69610f74faa606aa2423af86f518d0a4d87
Lewis Crawford <lewis@Lewiss-MacBook-Pro-2.local>
2018-06-27 16:38:28 +0100

    fully integrated with IPA

--- down.sh
+++ down.sh
@@ -1,2 +1,3 @@
 docker-compose  -p lbg -f examples/compose/ranger.yml down 
-docker volume rm lbg_dn0-data lbg_dn1-data lbg_dn2-data lbg_iparun lbg_ipatmp lbg_master0-data 
+docker volume rm lbg_dn0-data lbg_dn1-data lbg_dn2-data lbg_ipa-run lbg_ipa-tmp lbg_master0-data 
+sudo rm -rf /data/*
--- up.sh
+++ up.sh
@@ -1,1 +1,2 @@
+sudo cp containers/ipa/ipa-server-install-options /data
 docker-compose  -p lbg -f examples/compose/ranger.yml up
