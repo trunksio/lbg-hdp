a7da26569d64e01f89f6d8c9545fd85e2b1cae24
Lewis Crawford <lewis.crawford@dev-p102-app-01.sandbox.local>
2018-06-29 14:50:55 +0100

    Tidy up

--- containers/hdfsyarn/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sed -i "s/hostname=localhost/hostname=ambari-server.dev/" /etc/ambari-agent/conf/ambari-agent.ini
-ambari-agent start
-
-while true; do
-  sleep 3
-  tail -f /var/log/ambari-agent/ambari-agent.log
-done
