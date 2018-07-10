0cdbc8c4d3cf8e949be8dae80b44486eea7b84a6
Crawford <8404494@GLOBAL.LLOYDSTSB.COM>
2018-07-10 10:56:00 +0100

    changed ipa data volume

--- down.sh
+++ down.sh
@@ -1,3 +1,2 @@
 docker-compose  -p lbg -f examples/compose/ranger.yml down 
 docker volume rm lbg_dn0-data lbg_dn1-data lbg_dn2-data lbg_ipa-run lbg_ipa-tmp lbg_master0-data 
-sudo rm -rf /data/*
--- up.sh
+++ up.sh
@@ -1,2 +1,1 @@
-sudo cp containers/ipa/ipa-server-install-options /data
 docker-compose  -p lbg -f examples/compose/ranger.yml up
